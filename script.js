254f8ac8e9f26a6524967fb1bdaed90fffbdc091
YACOUBA ASSOUMANE Moubarak <98525444+Mbktec@users.noreply.github.com>
2022-06-07 13:25:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,172 +1,112 @@
-     
-     
-    let erreurBudget = document.querySelector(".erreurBudget");
-    let erreurExpense = document.querySelector(".erreurExpense");
-    let budgetForm = document.getElementById("budget-form");
-    let budgetInput = document.getElementById("budget-input");
-    let budgetTotal = document.getElementById("budgetTotal");
-    let expenseAmount = document.getElementById("expense-amount");
-    let balance = document.getElementById("balance");
-    let balanceAmount = document.getElementById("balance-amount");
-    let expenseForm = document.getElementById("expense-form");
-    let expenseInput = document.getElementById("expense-input");
-    let amountInput = document.getElementById("amount-input");
-    let expenseList = document.getElementById("expense-list");
-    let itemList = [];
-    let itemID = 0;
+const erreurBudget = document.querySelector(".erreurBudget");
+const erreurExpense = document.querySelector(".erreurExpense");
+const budgetInput = document.getElementById('budgetInput');
+const expenseInput = document.getElementById('expenseInput');
+const amountInput = document.getElementById('amountInput');
+const balanceTotal = document.getElementById('balanceTotal');
+const balanceExpense = document.getElementById('balanceExpense');
+const balanceValue = document.getElementById('balanceValue');
+const btnExpense = document.getElementById('addExpense');
+const btncal= document.getElementById('calElem');
+const container = document.getElementById('container')
+let budget=0
+let expenTotal =[];
+let gaspil =0;
+let i = 0;
 
-
-    //valeur du budget
-    budgetForm.addEventListener("submit",
-    function  (event){
-      event.preventDefault();
-        const value = budgetInput.value;
-        if (value ===""|| value <  0){
-          erreurBudget.style.display = 'block';
-          budgetInput.value = ""
-          setTimeout(function() {
-            erreurBudget.style.display = 'none';
-          }, 3000);
-
+ btncal.addEventListener('click', (e)=>{
+     e.preventDefault()
+     const value = budgetInput.value;
+     if (value ===""|| value <  0){
+        erreurBudget.style.display = 'block';
+        budgetInput.value = ""
+        setTimeout(function() {
+          erreurBudget.style.display = 'none';
+        }, 3000);
+        }else {
+            budget = budgetInput.value;
+            balanceTotal.textContent= budget;
+            balanceValue.textContent = budget;
+            budgetInput.value="";
         }
-        else{
-         
-          budgetTotal.textContent = value;
-         
-          budgetInput.value = "";
-          showBalance();
-        }
-    })
+   
+ });
 
-    function showBalance(){
-      const expense = totalExpense();
-      const total = parseInt(budgetTotal.textContent)-expense;
-      balanceAmount.textContent = total;
-    }
+
+ btnExpense.addEventListener('click', (e) =>{
+     e.preventDefault();const expenseValue = expenseInput.value;
+     const amountValue = amountInput.value;
+     if(expenseValue ===""||amountValue===""|| amountValue <0){
+       erreurExpense.style.display ='block';
+       setTimeout(function() {
+         erreurExpense.style.display ='none';
+       }, 3000);
+    }else{
 
    
-
-    // expense submit
-    expenseForm.addEventListener("submit",
-     function(event){
-       event.preventDefault();
-      const expenseValue = expenseInput.value;
-      const amountValue = amountInput.value;
-      if(expenseValue ===""||amountValue===""|| amountValue <0){
-        erreurExpense.style.display ='block';
-        setTimeout(function() {
-          erreurExpense.style.display ='none';
-        }, 3000);
-
-      }
-      else{
-        let amount = parseInt(amountValue);
-        expenseInput.value ="";
-        amountValue.value = "";
-        amountInput.value = "";
-        
-        let expense = {
-          id: itemID,
-          title:expenseValue,
-          amount:amount
-        };
-        itemID++;
-        itemList.push(expense);
-        addExpense(expense);
-        showBalance();
-      }
-     });
+     expenTotal =[]
+     expenTotal.push({nom:expenseInput.value,prix:amountInput.valueAsNumber})
+    gaspil += amountInput.valueAsNumber;
+    balanceExpense.textContent = gaspil;
+    balanceValue.textContent = budget - gaspil;
+     expenseInput.value = "";
+     amountInput.value = "";
     
      
-    function addExpense(expense){
-      const div = document.createElement("div");
-      div.classList.add("expense");
-      div.innerHTML = 
-      `<div class="expense-item d-flex justify-content-between align-items-baseline">
-       <h6 class="expense-title mb-0 text-uppercase list-item"> ${
-         expense.title
-       }</h6>
-       <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
-       <!-- icons -->
-      <div class="expense-icons list-item">
-          <a href="#" class="edit-icon text-success mx-2" data-id="${expense.id}">
-           <i class="fas fa-edit"></i>
-          </a>
-          <a href="#" class="delete-icon text-danger" data-id="${expense.id}">
-           <i class="fas fa-trash"></i>
-          </a>
-         </div>
-      </div>
-   `;
-   expenseList.appendChild(div);
-    }
-     //calcule de la depense total
-     function totalExpense(){
-      let total = 0;
-      if (itemList.length > 0){
-        total = itemList.reduce(function(prev,curr){
-          prev += curr.amount;
-          return prev;
-        },0)
-        HTMLHtmlElement
-      }
-      expenseAmount.textContent = total;
-      return total;
-    }
-
-    //bouton edit expense
-function editExpense(element){
-  let id = parseInt(element.dataset.id);
-  let parent = element.parentElement.parentElement.parentElement;
-  // supression du DOM
-  expenseList.removeChild(parent);
-  // supression de la liste 
-  let expense = itemList.filter(function(item){
-    return item.id === id;
-  });
-
-  //afficher la valeur a edité
-  expenseInput.value = expense[0].title;
-  amountInput.value = expense[0].amount;
-  //suppression de l'item
-  let supList = itemList.filter(function(expense){
-    return expense.id !== id;
-  });
-  itemList = supList;
-  showBalance();
-}
-
-
-// supression de la depense
-function deleteExpense (element){
-  let id = parseInt(element.dataset.id);
-  //console.log(id);
-  let parent = element.parentElement.parentElement.parentElement;
-  // supression du DOM
-  expenseList.removeChild(parent);
-  // supression de item
-  let supItem = itemList.filter(function(expense){
-    return expense.id !== id;
-  });
-  itemList = supItem;
-  showBalance();
-
-}
-expenseList.addEventListener("click" ,function(event){
-  if(event.target.parentElement.classList.contains("edit-icon")){
-    editExpense(event.target.parentElement);
-  }
-  else if (event.target.parentElement.classList.contains("delete-icon")){
-    deleteExpense(event.target.parentElement);
-  }
-
-});
-
-    
+    expenTotal.forEach(tablo =>
+    container.innerHTML+=`  
+    <div class="row">
+    <div class="col">
+    <p>${tablo.nom}</p>
+    <span class="expenseTitle"></span>
+    </div>
+    <div class="col">
+        <p>${tablo.prix}</p>
+        <span class="expenseValue"></span>
+    </div>
+    <div class="col">
+    <button class="btn deleteEl" data-index ="${i}"><i class="bi bi-trash3-fill text-danger"></i></button>
+        <button class="btn editEl" data-index ="${i}"><i class="bi bi-pencil-fill text-success"></i></button>
+    </div>
+           </div>
+        `
+       
+    ) }
+    i++
+    console.log(i)
+ 
+        
 
 
 
 
 
 
-  
+
+container.querySelectorAll(".deleteEl").forEach(btn=>
+         btn.addEventListener("click",()=>{        
+            let supEl= btn.parentNode.parentNode
+            let valMoin = parseInt(supEl.children[1].children[0].textContent)         
+            gaspil -= valMoin;
+            balanceExpense.textContent = gaspil;
+            balanceValue.textContent = budget - gaspil;
+            container.removeChild(supEl)
+        })
+        )
+container.querySelectorAll(".editEl").forEach(btn=>
+         btn.addEventListener("click",()=>{          
+            let supEl= btn.parentNode.parentNode
+            let valMoin = parseInt(supEl.children[1].children[0].textContent)
+            gaspil -= valMoin;
+            balanceExpense.textContent = gaspil;
+            balanceValue.textContent = budget - gaspil;           
+            expenseInput.value =supEl.children[0].children[0].textContent;
+            amountInput.value = valMoin;
+            container.removeChild(supEl)           
+        })
+        )
+ })
+
+
+
+
